78eebbe3d5684cae00084ec83efbf8a99265ea98
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-05-28 16:58:37 +0300

    add dist

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _readlineSync = require('readline-sync');
+
+var _readlineSync2 = _interopRequireDefault(_readlineSync);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var urerName = _readlineSync2.default.question('May I have your name?: ');
+exports.default = urerName;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJ1cmVyTmFtZSIsInJlYWRsaW5lU3luYyIsInF1ZXN0aW9uIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTs7Ozs7O0FBQ0EsSUFBTUEsV0FBV0MsdUJBQWFDLFFBQWIsQ0FBc0IseUJBQXRCLENBQWpCO2tCQUNlRixRIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHJlYWRsaW5lU3luYyBmcm9tICdyZWFkbGluZS1zeW5jJztcbmNvbnN0IHVyZXJOYW1lID0gcmVhZGxpbmVTeW5jLnF1ZXN0aW9uKCdNYXkgSSBoYXZlIHlvdXIgbmFtZT86ICcpO1xuZXhwb3J0IGRlZmF1bHQgdXJlck5hbWU7Il19
