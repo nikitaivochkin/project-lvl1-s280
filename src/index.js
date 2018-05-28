9d361e1eac3f095a5c7fa6b0f09b0cb2bd8808c6
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-05-28 23:29:52 +0300

    fix all problem (with eslint)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
+
 const questionOfName = () => {
-    console.log(`Hello, ${readlineSync.question('May I have your name?: ')}!`)
+  console.log(`Hello, ${readlineSync.question('May I have your name?: ')}!`);
 };
 export default questionOfName;
