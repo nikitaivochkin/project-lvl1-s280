8f6fa69a190d4a5cf99f34117b30d1d7236a366b
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-05-28 16:28:41 +0300

    deleеу unnecessary catalog

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,4 @@
-#!/usr/bin/env nod
-console.log('Welcome to the Brain Games!')
+#!/usr/bin/env node
+console.log('Welcome to the Brain Games!');
+import urerName from "..";
+console.log(`Hello, ${urerName}!`)
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import readlineSync from 'readline-sync';
-const actual = readlineSync.question('May I have your name?: ');
+const urerName = readlineSync.question('May I have your name?: ');
+export default urerName;
