4393668a991bdbf84bb854d09e8088f7f0d21d0b
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-05-28 18:44:03 +0300

    fix small sintaxis

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import questionOfName from ".."
+import questionOfName from "..";
 console.log('Welcome to the Brain Games!');
 questionOfName();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 const questionOfName = () => {
     console.log(`Hello, ${readlineSync.question('May I have your name?: ')}!`)
-}
+};
 export default questionOfName;
