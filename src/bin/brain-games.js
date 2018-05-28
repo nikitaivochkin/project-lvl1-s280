74644da5ab37db58df08163d30dd23d41413443b
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-05-28 18:30:57 +0300

    add funck questionOfName

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
+import questionOfName from ".."
 console.log('Welcome to the Brain Games!');
-import urerName from "..";
-console.log(`Hello, ${urerName}!`)
+questionOfName();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import readlineSync from 'readline-sync';
-const urerName = readlineSync.question('May I have your name?: ');
-export default urerName;
+const questionOfName = () => {
+    console.log(`Hello, ${readlineSync.question('May I have your name?: ')}!`)
+}
+export default questionOfName;
