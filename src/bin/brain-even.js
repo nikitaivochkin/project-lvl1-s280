2d327415fb3886a4f7bdd859eb4f7aa28b24b04a
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-05-29 11:36:24 +0300

    work with code

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import questionOfName from '..';
+
 console.log('Welcome to the Brain Games!');
 console.log('Answer "yes" if number even otherwise answer "no".');
+questionOfName();
