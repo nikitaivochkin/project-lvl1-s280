5478c2537e5f51a26a1d5d6f72abbdc227d22e89
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-05-30 16:27:33 +0300

    add \n

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,9 +3,9 @@ import { iter } from '..';
 
 const brainEven = () => {
   console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no".');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
   const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}!\n`);
   return iter(name, 2);
 };
 export default brainEven;
