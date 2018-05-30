7ee9086094975baf1271ff922c004086f8bcfce5
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-05-30 22:52:39 +0300

    fix remarks of mentor

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,5 +1,21 @@
 import readlineSync from 'readline-sync';
-import { iter } from '..';
+import { generateNumber, isEvenAnswer } from '../functions';
+
+export const iter = (name, counter) => {
+  const question = generateNumber(100, 1);
+  console.log(`Question: ${question}!`);
+  const rightAnswer = isEvenAnswer(question) ? 'yes' : 'no';
+  const usersAnswer = readlineSync.question('Your answer: ');
+  if (counter === 0 && rightAnswer === usersAnswer) {
+    console.log('Correct!');
+    return console.log(`Congratulations, ${name}!`);
+  } else if (rightAnswer === usersAnswer) {
+    console.log('Correct!');
+    return iter(name, counter - 1);
+  }
+  console.log(`'${usersAnswer}' is wrong answer ;(.Correct answer was '${rightAnswer}'.`);
+  return console.log(`Let's try again, ${name}!`);
+};
 
 const brainEven = () => {
   console.log('Welcome to the Brain Games!');
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
-import { generateNumber, isEvenAnswer } from './functions';
 
 const questionName = () => {
   console.log('Welcome to the Brain Games!');
@@ -9,20 +8,3 @@ const questionName = () => {
 };
 export default questionName;
 
-export const iter = (name, counter) => {
-  const question = generateNumber(100, 1);
-  console.log(`Question: ${question}!`);
-  const rightAnswer = isEvenAnswer(question) ? 'yes' : 'no';
-  const usersAnswer = readlineSync.question('Your answer: ');
-  if (counter === 0 && rightAnswer === usersAnswer) {
-    console.log('Correct!');
-    return console.log(`Congratulations, ${name}!`);
-  } else if (rightAnswer === usersAnswer) {
-    console.log('Correct!');
-    return iter(name, counter - 1);
-  } else if (rightAnswer !== usersAnswer) {
-    console.log(`'${usersAnswer}' is wrong answer ;(.Correct answer was '${rightAnswer}'.`);
-    return console.log(`Let's try again, ${name}!`);
-  }
-  return iter(name, 2);
-};
