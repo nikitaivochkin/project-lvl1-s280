19530d921e1cd426ac9e0af9eb87e402579da778
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-05-31 10:55:03 +0300

    add +, -, *

--- src/games/brain-clac.js
+++ src/games/brain-clac.js
@@ -1,12 +1,20 @@
 import readlineSync from 'readline-sync';
+import { cons, car, cdr } from 'hexlet-pairs';
 import { generateNumber, generateNumber2 } from '../functions';
+
+const getQuestions = (a, b, counter) => {
+  if (counter === 2) {
+    return cons((`${a} + ${b}`), String(a + b));
+  } else if (counter === 1) {
+    return cons((`${a} - ${b}`), String(a - b));
+  } return cons((`${a} * ${b}`), String(a * b));
+};
 
 const iter = (name, counter) => {
   const randomNum = generateNumber(100, 1);
   const randomNum2 = generateNumber2(100, 1);
-  const question = `${randomNum} + ${randomNum2}`;
-  console.log(`Question: ${question}`);
-  const rightAnswer = String(randomNum + randomNum2);
+  console.log(`Question: ${car(getQuestions(randomNum, randomNum2, counter))}`);
+  const rightAnswer = cdr(getQuestions(randomNum, randomNum2, counter));
   const usersAnswer = readlineSync.question('Your answer: ');
   if (counter === 0 && rightAnswer === usersAnswer) {
     console.log('Correct!');
