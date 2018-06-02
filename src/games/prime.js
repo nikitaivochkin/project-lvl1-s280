83f385e763a28ef1d8a47d97a6a3a2d0159af8bc
Никита Ивочкин <nikita.ivochkin@icloud.com>
2018-06-02 21:39:03 +0300

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,31 @@
+import { cons } from 'hexlet-pairs';
+import generateNumber from '../utils';
+import startGame from '../controlOfGames';
+
+const isEvenAnswer = num => (num % 2 === 0);
+
+const isPrime = (num) => {
+  let result = 2;
+  if (num === 1 || num < 1) {
+    return false;
+  }
+  for (let counter = 1; num % result !== 0; counter) {
+    result += counter;
+  }
+  if (result === num) {
+    return true;
+  }
+  return false;
+};
+
+const generatorDataForStartGame = () => {
+  const question = generateNumber(200, 1);
+  const rightAnswer = isPrime(question) ? 'yes' : 'no';
+  return cons(question, rightAnswer);
+};
+
+const task = 'Answer "yes" if number is prime otherwise answer "no".';
+
+const brainEven = () => startGame(generatorDataForStartGame, task);
+
+export default brainEven;
